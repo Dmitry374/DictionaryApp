d435c6a22c5e474ea1ff1a1386a8f5506e9127e0
Dima <dmitry.turov374@gmail.com>
2021-03-22 00:34:47 +0300

    switch dictionary language

--- app/src/main/java/com/example/dictionaryapp/ui/words/WordsFragment.kt
+++ app/src/main/java/com/example/dictionaryapp/ui/words/WordsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -19,6 +20,10 @@ import javax.inject.Inject
 
 class WordsFragment : Fragment() {
 
+    companion object {
+        const val LANGUAGE_KEY = "language_key"
+    }
+
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
@@ -29,6 +34,8 @@ class WordsFragment : Fragment() {
     private val wordsAdapter = WordsAdapter { word ->
 
     }
+
+    private var wordsLanguage = Constants.LANGUAGE_EN
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -47,8 +54,38 @@ class WordsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        wordsLanguage = savedInstanceState?.getString(LANGUAGE_KEY) ?: Constants.LANGUAGE_EN
+
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = resources.getString(R.string.words)
+
+        toolbar.inflateMenu(R.menu.words_menu)
+
+        toolbar.setOnMenuItemClickListener(Toolbar.OnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.en_ru -> {
+
+                    if (wordsLanguage != Constants.LANGUAGE_EN) {
+                        wordsLanguage = Constants.LANGUAGE_EN
+                        wordsViewModel.loadAllWordsByLanguage(wordsLanguage)
+                    }
+                    return@OnMenuItemClickListener true
+                }
+                R.id.ru_en -> {
+                    if (wordsLanguage != Constants.LANGUAGE_RU) {
+                        wordsLanguage = Constants.LANGUAGE_RU
+                        wordsViewModel.loadAllWordsByLanguage(wordsLanguage)
+                    }
+                    return@OnMenuItemClickListener true
+                }
+                else -> {
+                    return@OnMenuItemClickListener true
+                }
+            }
+        })
+
         if (savedInstanceState == null) {
-            wordsViewModel.loadAllWordsByLanguage(Constants.LANGUAGE_EN)
+            wordsViewModel.loadAllWordsByLanguage(wordsLanguage)
         }
 
         recyclerWords.layoutManager = LinearLayoutManager(activity)
@@ -57,12 +94,20 @@ class WordsFragment : Fragment() {
         wordsViewModel.words.observe(viewLifecycleOwner, Observer {
             it?.let { words ->
                 if (words.isEmpty()) {
+                    recyclerWords.visibility = View.GONE
                     emptyWordsListMessage.visibility = View.VISIBLE
                 } else {
                     emptyWordsListMessage.visibility = View.GONE
+                    recyclerWords.visibility = View.VISIBLE
                     wordsAdapter.submitList(words)
                 }
             }
         })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(LANGUAGE_KEY, wordsLanguage)
     }
 }
