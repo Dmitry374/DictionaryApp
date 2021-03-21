940312c23ca463d5fc51197fbb6d4cbdde9d4363
Dima <dmitry.turov374@gmail.com>
2021-03-21 16:28:04 +0300

    add words fragment

--- app/src/main/java/com/example/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/example/dictionaryapp/MainActivity.kt
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentManager = supportFragmentManager
+
+    private val moviesFragment = WordsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            fragmentManager.beginTransaction()
+                .replace(R.id.nav_host_container, moviesFragment)
+                .commit()
+        }
     }
 }
